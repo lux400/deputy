09e136d95b914af0cd253beba91a95cc62f5f6b2
lux400 <tsimbalenko.info@gmail.com>
2020-02-05 16:35:06 +0200

    test cyrillic

--- gatsby-node.js
+++ gatsby-node.js
@@ -34,7 +34,7 @@ exports.createPages = ({ actions, graphql }) => {
     posts.forEach(edge => {
       const id = edge.node.id;
       createPage({
-        path: edge.node.fields.slug,
+        path: cyrillicToTranslit.transform(edge.node.fields.slug, "-"),
         component: path.resolve(
           `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
         ),
