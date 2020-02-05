c49717bec84a83781487c5aa195b7d741a6ef2f0
lux400 <tsimbalenko.info@gmail.com>
2020-02-05 17:50:46 +0200

    test custom .md names

--- gatsby-node.js
+++ gatsby-node.js
@@ -35,6 +35,9 @@ exports.createPages = ({ actions, graphql }) => {
       const id = edge.node.id;
       createPage({
         path: cyrillicToTranslit.transform(edge.node.fields.slug, "-"),
+
+
+
         component: path.resolve(
           `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
         ),
